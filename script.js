02512f086fd3f7be74581b8b5ded0f75a9ce8fd8
Hossein Douzandeh <Douzandeh.k@email.come>
2023-01-17 19:24:58 +0330

    AD=>Clock.js Convert Gregorian date to solar

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 // let mytime = new Date()
-// console.log( `Date: ${mytime.getDate()}` ); 
-// console.log( `Day: ${mytime.getDay()}` ); 
-// console.log( `FullYear: ${mytime.getFullYear()}` ); 
-// console.log( `ours: ${mytime.getHours()}` ); 
-// console.log( `Milliseconds: ${mytime.getMilliseconds()}` ); 
-// console.log( `Minutes: ${mytime.getMinutes()}` ); 
-// console.log( `Month: ${mytime.getMonth()}` ); 
-// console.log( `Seconds: ${mytime.getSeconds()}` ); 
-// console.log( `Time: ${mytime.getTime()}` ); 
+// console.log( `Date: ${mytime.getDate()}` );
+// console.log( `Day: ${mytime.getDay()}` );
+// console.log( `FullYear: ${mytime.getFullYear()}` );
+// console.log( `ours: ${mytime.getHours()}` );
+// console.log( `Milliseconds: ${mytime.getMilliseconds()}` );
+// console.log( `Minutes: ${mytime.getMinutes()}` );
+// console.log( `Month: ${mytime.getMonth()}` );
+// console.log( `Seconds: ${mytime.getSeconds()}` );
+// console.log( `Time: ${mytime.getTime()}` );
 
-let myClock = () =>{
-    let clock = document.querySelector("#clock")
-    let myDate = new Date()
-    clock.textContent = `${myDate.getHours()}:${myDate.getMinutes()}:${myDate.getSeconds()}`
-}
-myClock()
+// let myClock = () =>{
+//     let clock = document.querySelector("#clock")
+//     let myDate = new Date()
+//     clock.textContent = `${myDate.getHours()}:${myDate.getMinutes()}:${myDate.getSeconds()}`
+// }
+// myClock()
 
-setInterval(myClock , 1000)
+// setInterval(myClock , 1000)
+
+let clock = document.querySelector("#clock");
+let myDate = new Date();
+let myClock = gregorian_to_jalali(
+    myDate.getFullYear(),
+    myDate.getMonth() + 1,
+    myDate.getDate()
+)
+clock.textContent = `امسال سال ${myClock[0]} برج ${myClock[1]} و روز ${myClock[2]}`
