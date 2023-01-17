f63a6292190d89a05b34755f1edc56bd9e69e304
Hossein Douzandeh <Douzandeh.k@email.come>
2023-01-17 19:00:02 +0330

    h,m,s

--- script.js
+++ script.js
@@ -12,7 +12,8 @@
 let myClock = () =>{
     let clock = document.querySelector("#clock")
     let myDate = new Date()
-    clock.textContent = myDate
+    clock.textContent = `${myDate.getHours()}:${myDate.getMinutes()}:${myDate.getSeconds()}`
 }
+myClock()
 
 setInterval(myClock , 1000)
